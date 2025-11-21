bd723cf7a95a482ef2f7b48e522ad9ff91d283a4
Nicko <nikos.tsagog@gmail.com>
2025-11-21 09:45:44 -0500

    set up first class
    guiding comments for thoughts
    - use linked lists for bfs

---
+++ src/chessboard.java
@@ -0,0 +1,6 @@
+public class chessboard {
+    
+    //make possible moves objects to store x and y coordinates
+    //breadth-first search & LinkedList
+
+}
---
+++ src/main.java
@@ -0,0 +1,12 @@
+ //Author: Nicko Tsagogeorgas
+ //Date: November __, 2025
+ //Problem: CCC '10 J5 - Knights' Hop
+ //Description: [Brief description of your approach]
+
+public class main{
+    public static void main(String[] args){
+        
+        //run commands here
+
+    }
+}
